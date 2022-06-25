75932158e9ea2920309001e947c4efa8197922aa
PatrickDP <patrick.d.pimenta@gmail.com>
2022-06-24 21:40:36 -0300

    Adicionando letra c / imagens de teste

--- python_code/letterB.py
+++ python_code/letterB.py
@@ -1,4 +1,4 @@
-''' RESOLUÇÃO DA LETRA B '''
+# RESOLUÇÃO DA LETRA B
 
 import numpy as np
 import cv2 as cv
@@ -12,7 +12,9 @@ def generate_histogram(img):
     plt.show()
     
 def remove_background(img, gaussianBlur_img, what_img):
-    final_img = 0
+    mask_name = 0
+    mask_img = []
+    final_img = []
     
     # PARA O CASO DA PRIMEIRA IMAGEM (JÁ LIMIARIZADA E PRECISA APENAS DO MÉTODO CLOSING)
     if(what_img == 1):
@@ -28,45 +30,20 @@ def remove_background(img, gaussianBlur_img, what_img):
         bitwise_img = cv.bitwise_or(img, erode_CEimg, mask=None)
         
         kernel_4 = cv.getStructuringElement(cv.MORPH_RECT, [1, 8])
+        
+        mask_name = "bolhas_mask.png"
         final_img = cv.morphologyEx(bitwise_img, cv.MORPH_CLOSE, kernel_4)
+        mask_img = final_img.copy()
                 
         plt.figure('bolhas.png')
         
-        plt.subplot(2, 3, 1)
-        plt.title('img')
-        plt.axis('OFF')
-        plt.plot()
-        plt.imshow(img, cmap='gray')
+        plt.subplot(2, 3, 1), plt.title('img'), plt.axis('OFF'), plt.plot(), plt.imshow(img, cmap='gray')
+        plt.subplot(2, 3, 2), plt.title('erode_img'), plt.axis('OFF'), plt.plot(), plt.imshow(erode_img, cmap='gray')
+        plt.subplot(2, 3, 3), plt.title('closing_Eimg'), plt.axis('OFF'), plt.plot(), plt.imshow(closing_Eimg, cmap='gray')
         
-        plt.subplot(2, 3, 2)
-        plt.title('erode_img')
-        plt.axis('OFF')
-        plt.plot()
-        plt.imshow(erode_img, cmap='gray')
-        
-        plt.subplot(2, 3, 3)
-        plt.title('closing_Eimg')
-        plt.axis('OFF')
-        plt.plot()
-        plt.imshow(closing_Eimg, cmap='gray')
-        
-        plt.subplot(2, 3, 4)
-        plt.title('erode_CEimg')
-        plt.axis('OFF')
-        plt.plot()
-        plt.imshow(erode_CEimg, cmap='gray')
-        
-        plt.subplot(2, 3, 5)
-        plt.title('bitwise_img')
-        plt.axis('OFF')
-        plt.plot()
-        plt.imshow(bitwise_img, cmap='gray')
-        
-        plt.subplot(2, 3, 6)
-        plt.title('final_image')
-        plt.axis('OFF')
-        plt.plot()
-        plt.imshow(final_img, cmap='gray')
+        plt.subplot(2, 3, 4), plt.title('erode_CEimg'), plt.axis('OFF'), plt.plot(), plt.imshow(erode_CEimg, cmap='gray')
+        plt.subplot(2, 3, 5), plt.title('bitwise_img'), plt.axis('OFF'), plt.plot(), plt.imshow(bitwise_img, cmap='gray')
+        plt.subplot(2, 3, 6), plt.title('final_image'), plt.axis('OFF'), plt.plot(), plt.imshow(final_img, cmap='gray')
         
         plt.show()      
     
@@ -84,64 +61,24 @@ def remove_background(img, gaussianBlur_img, what_img):
         
         bitwise_TOimg = cv.bitwise_xor(thresh_img, opening_Cimg, mask=None)
         bitwise_OBimg = cv.bitwise_or(opening_Cimg, bitwise_TOimg, mask=None)
-        
+    
+        mask_name = "coins-01_mask.jpg" 
+        mask_img = bitwise_OBimg.copy()
         final_img = cv.bitwise_and(img, img, mask=bitwise_OBimg)
         
         plt.figure('coins-01.jpg')
         
-        plt.subplot(3, 3, 1)
-        plt.title('gaussianBlur_img')
-        plt.axis('OFF')
-        plt.plot()
-        plt.imshow(gaussianBlur_img, cmap='gray')
+        plt.subplot(3, 3, 1), plt.title('gaussianBlur_img'), plt.axis('OFF'), plt.plot(), plt.imshow(gaussianBlur_img, cmap='gray')
+        plt.subplot(3, 3, 2), plt.title('canny_img'), plt.axis('OFF'), plt.plot(), plt.imshow(canny_img, cmap='gray')
+        plt.subplot(3, 3, 3), plt.title('thresh_img'), plt.axis('OFF'), plt.plot(), plt.imshow(thresh_img, cmap='gray')
         
-        plt.subplot(3, 3, 2)
-        plt.title('canny_img')
-        plt.axis('OFF')
-        plt.plot()
-        plt.imshow(canny_img, cmap='gray')
+        plt.subplot(3, 3, 4), plt.title('bitwise_img'), plt.axis('OFF'), plt.plot(), plt.imshow(bitwise_img, cmap='gray')
+        plt.subplot(3, 3, 5), plt.title('closing_img'), plt.axis('OFF'), plt.plot(), plt.imshow(closing_BCimg, cmap='gray')
+        plt.subplot(3, 3, 6), plt.title('opening_img'), plt.axis('OFF'), plt.plot(), plt.imshow(opening_Cimg, cmap='gray')
         
-        plt.subplot(3, 3, 3)
-        plt.title('thresh_img')
-        plt.axis('OFF')
-        plt.plot()
-        plt.imshow(thresh_img, cmap='gray')
-        
-        plt.subplot(3, 3, 4)
-        plt.title('bitwise_img')
-        plt.axis('OFF')
-        plt.plot()
-        plt.imshow(bitwise_img, cmap='gray')
-        
-        plt.subplot(3, 3, 5)
-        plt.title('closing_img')
-        plt.axis('OFF')
-        plt.plot()
-        plt.imshow(closing_BCimg, cmap='gray')
-        
-        plt.subplot(3, 3, 6)
-        plt.title('opening_img')
-        plt.axis('OFF')
-        plt.plot()
-        plt.imshow(opening_Cimg, cmap='gray')
-        
-        plt.subplot(3, 3, 7)
-        plt.title('bitwise_TOimg')
-        plt.axis('OFF')
-        plt.plot()
-        plt.imshow(bitwise_TOimg, cmap='gray')
-        
-        plt.subplot(3, 3, 8)
-        plt.title('bitwise_OBimg')
-        plt.axis('OFF')
-        plt.plot()
-        plt.imshow(bitwise_OBimg, cmap='gray')
-        
-        plt.subplot(3, 3, 9)
-        plt.title('final_img')
-        plt.axis('OFF')
-        plt.plot()
-        plt.imshow(final_img, cmap='gray')
+        plt.subplot(3, 3, 7), plt.title('bitwise_TOimg'), plt.axis('OFF'), plt.plot(), plt.imshow(bitwise_TOimg, cmap='gray')
+        plt.subplot(3, 3, 8), plt.title('bitwise_OBimg'), plt.axis('OFF'), plt.plot(), plt.imshow(bitwise_OBimg, cmap='gray')
+        plt.subplot(3, 3, 9), plt.title('final_img'), plt.axis('OFF'), plt.plot(), plt.imshow(final_img, cmap='gray')
         
         plt.show()
     # ... TERCEIRA IMAGEM (USANDO LIMIAR ADAPTATIVO E O MÉTODO OPENING)
@@ -151,37 +88,22 @@ def remove_background(img, gaussianBlur_img, what_img):
         kernel = cv.getStructuringElement(cv.MORPH_CROSS, [3, 3])
         opening_img = cv.morphologyEx(thresh_img, cv.MORPH_OPEN, kernel)
         
+        mask_name = "rice_mask.png"
+        mask_img = opening_img.copy()
         final_img = cv.bitwise_and(img, img, mask=opening_img)
         
         plt.figure('rice.png')
         
-        plt.subplot(2, 2, 1)
-        plt.title('gaussianBlur_img')
-        plt.axis('OFF')
-        plt.plot()
-        plt.imshow(gaussianBlur_img, cmap='gray')
+        plt.subplot(2, 2, 1), plt.title('gaussianBlur_img'), plt.axis('OFF'), plt.plot(), plt.imshow(gaussianBlur_img, cmap='gray')
+        plt.subplot(2, 2, 2), plt.title('thresh_img'), plt.axis('OFF'), plt.plot(), plt.imshow(thresh_img, cmap='gray')
         
-        plt.subplot(2, 2, 2)
-        plt.title('thresh_img')
-        plt.axis('OFF')
-        plt.plot()
-        plt.imshow(thresh_img, cmap='gray')
-        
-        plt.subplot(2, 2, 3)
-        plt.title('opening_img')
-        plt.axis('OFF')
-        plt.plot()
-        plt.imshow(opening_img, cmap='gray')
-        
-        plt.subplot(2, 2, 4)
-        plt.title('final_img')
-        plt.axis('OFF')
-        plt.plot()
-        plt.imshow(final_img, cmap='gray')
+        plt.subplot(2, 2, 3), plt.title('opening_img'), plt.axis('OFF'), plt.plot(), plt.imshow(opening_img, cmap='gray')
+        plt.subplot(2, 2, 4), plt.title('final_img'), plt.axis('OFF'), plt.plot(), plt.imshow(final_img, cmap='gray')
         
         plt.show()
     else:
         print('ERROR - RETURNING FUNCTION...')
         return
     
+    cv.imwrite('output_images/%s' %(mask_name), mask_img)
     return final_img
--- python_code/main.py
+++ python_code/main.py
@@ -3,6 +3,7 @@ import cv2 as cv
 import matplotlib.pyplot as plt
 
 import letterB as b
+import letterC as c
 
 # CASO 1, 2, 3 
 img = cv.imread('test_images/' + 'bolhas.png')[:,:,::-1]
@@ -21,50 +22,17 @@ gaussianBlur_img3 = cv.GaussianBlur(gray_img3, [3, 3], 0)
 
 plt.figure('images')
 
-plt.subplot(3, 3, 1)
-plt.title('img')
-plt.axis('OFF')
-plt.imshow(img)
+plt.subplot(3, 3, 1), plt.title('img'), plt.axis('OFF'), plt.imshow(img)
+plt.subplot(3, 3, 2), plt.title('gray_img'), plt.axis('OFF'), plt.imshow(gray_img, cmap='gray')
+plt.subplot(3, 3, 3), plt.title('gaussianBlur_img'), plt.axis('OFF'), plt.imshow(gaussianBlur_img, cmap='gray')
 
-plt.subplot(3, 3, 2)
-plt.title('gray_img')
-plt.axis('OFF')
-plt.imshow(gray_img, cmap='gray')
+plt.subplot(3, 3, 4), plt.title('img2'), plt.axis('OFF'), plt.imshow(img2)
+plt.subplot(3, 3, 5), plt.title('gray_img2'), plt.axis('OFF'), plt.imshow(gray_img2, cmap='gray')
+plt.subplot(3, 3, 6), plt.title('gaussianBlur_img2'), plt.axis('OFF'), plt.imshow(gaussianBlur_img2, cmap='gray')
 
-plt.subplot(3, 3, 3)
-plt.title('gaussianBlur_img')
-plt.axis('OFF')
-plt.imshow(gaussianBlur_img, cmap='gray')
-
-plt.subplot(3, 3, 4)
-plt.title('img2')
-plt.axis('OFF')
-plt.imshow(img2)
-
-plt.subplot(3, 3, 5)
-plt.title('gray_img2')
-plt.axis('OFF')
-plt.imshow(gray_img2, cmap='gray')
-
-plt.subplot(3, 3, 6)
-plt.title('gaussianBlur_img2')
-plt.axis('OFF')
-plt.imshow(gaussianBlur_img2, cmap='gray')
-
-plt.subplot(3, 3, 7)
-plt.title('img3')
-plt.axis('OFF')
-plt.imshow(img3)
-
-plt.subplot(3, 3, 8)
-plt.title('gray_img3')
-plt.axis('OFF')
-plt.imshow(gray_img3, cmap='gray')
-
-plt.subplot(3, 3, 9)
-plt.title('gaussianBlur_img3')
-plt.axis('OFF')
-plt.imshow(gaussianBlur_img3, cmap='gray')
+plt.subplot(3, 3, 7), plt.title('img3'), plt.axis('OFF'), plt.imshow(img3)
+plt.subplot(3, 3, 8), plt.title('gray_img3'), plt.axis('OFF'), plt.imshow(gray_img3, cmap='gray')
+plt.subplot(3, 3, 9), plt.title('gaussianBlur_img3'), plt.axis('OFF'), plt.imshow(gaussianBlur_img3, cmap='gray')
 
 plt.show()
 
@@ -72,17 +40,28 @@ cv.imwrite('test_images/gaussianBlur_bolhas.png', gaussianBlur_img)
 cv.imwrite('test_images/gaussianBlur_coins-01.jpg', gaussianBlur_img2)
 cv.imwrite('test_images/gaussianBlur_rice.png', gaussianBlur_img3)
 
-
 # B: RESPONSÁVEL EM REMOVER O FUNDO DA IMAGEM.
-noBackground_image = b.remove_background(img, False, 1)
-noBackground_image2 = b.remove_background(img2, gaussianBlur_img2, 2)
-noBackground_image3 = b.remove_background(img3, gaussianBlur_img3, 3)
+noBackground_img = b.remove_background(img, False, 1)
+noBackground_img2 = b.remove_background(img2, gaussianBlur_img2, 2)
+noBackground_img3 = b.remove_background(img3, gaussianBlur_img3, 3)
 
-noBackground_image = cv.cvtColor(noBackground_image, cv.COLOR_RGBA2BGRA)
-noBackground_image2 = cv.cvtColor(noBackground_image2, cv.COLOR_RGB2BGR)
-noBackground_image3 = cv.cvtColor(noBackground_image3, cv.COLOR_RGBA2BGRA)
+noBackground_img = cv.cvtColor(noBackground_img, cv.COLOR_RGBA2BGRA)
+noBackground_img2 = cv.cvtColor(noBackground_img2, cv.COLOR_RGB2BGR)
+noBackground_img3 = cv.cvtColor(noBackground_img3, cv.COLOR_RGBA2BGRA)
 
-cv.imwrite('output_images/bolhas_result.png', noBackground_image)
-cv.imwrite('output_images/coins-01_result.jpg', noBackground_image2)
-cv.imwrite('output_images/rice_result.png', noBackground_image3)
+cv.imwrite('output_images/bolhas_result.png', noBackground_img)
+cv.imwrite('output_images/coins-01_result.jpg', noBackground_img2)
+cv.imwrite('output_images/rice_result.png', noBackground_img3)
 
+# C: UTILIZA O ALGORITMO DE IMUNDAÇÃO
+thresh_img =  cv.imread('test_images/bolhas.png')
+# thresh_img2 =  cv.imread('output_images/coins-01_mask.jpg')
+# thresh_img3 =  cv.imread('output_images/rice_mask.png')
+
+thresh_img = cv.cvtColor(thresh_img, cv.COLOR_BGR2GRAY)
+# thresh_img2 = cv.cvtColor(thresh_img2, cv.COLOR_BGR2GRAY)
+# thresh_img3 = cv.cvtColor(thresh_img3, cv.COLOR_BGR2GRAY)
+
+label_img1 = c.labeling_breadthSearch(thresh_img)
+# label_img2 = c.labesling_breadthSearch(thresh_img2)
+# label_img3 = c.labeling_breadthSearch(thresh_img3)
